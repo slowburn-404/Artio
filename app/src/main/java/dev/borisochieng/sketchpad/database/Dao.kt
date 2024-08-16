455990be4068330b9f9b44e972a8d0980000b529
Roland <rolandnnamodi@gmail.com>
2024-08-16 13:15:12 +0100

    Updated README.md

--- app/src/main/java/dev/borisochieng/sketchpad/database/Dao.kt
+++ app/src/main/java/dev/borisochieng/sketchpad/database/Dao.kt
@@ -3,6 +3,7 @@ package dev.borisochieng.sketchpad.database
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
@@ -16,10 +17,10 @@ interface SketchDao {
 	@Query("SELECT * FROM sketch WHERE id LIKE :sketchId")
 	fun getSketch(sketchId: String): Flow<Sketch>
 
-	@Insert
+	@Insert(onConflict = OnConflictStrategy.REPLACE)
 	suspend fun saveSketch(sketch: Sketch)
 
-	@Insert
+	@Insert(onConflict = OnConflictStrategy.REPLACE)
 	suspend fun insertSketches(sketches: List<Sketch>)
 
 	@Update
