0942a212073453e5757e07639c3aef179d580976
Roland <rolandnnamodi@gmail.com>
2024-08-30 18:49:56 +0100

    tweaked home screen top bar: displaying default one for guest users and personalized one when logged in

--- app/src/main/java/dev/borisochieng/artio/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/dev/borisochieng/artio/ui/screens/home/HomeScreen.kt
@@ -10,14 +10,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Brush
 import androidx.compose.material.icons.rounded.History
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LinearProgressIndicator
@@ -39,7 +37,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -49,6 +46,7 @@ import androidx.compose.ui.unit.sp
 import com.google.firebase.auth.FirebaseAuth
 import dev.borisochieng.artio.database.Sketch
 import dev.borisochieng.artio.ui.components.Header
+import dev.borisochieng.artio.ui.components.HomeTopBar
 import dev.borisochieng.artio.ui.navigation.Screens
 import dev.borisochieng.artio.ui.screens.dialog.ItemMenuSheet
 import dev.borisochieng.artio.utils.ShimmerBoxItem
@@ -67,11 +65,15 @@ fun HomeScreen(
     val selectedSketch = remember { mutableStateOf<Sketch?>(null) }
     val snackbarHostState = remember { SnackbarHostState() }
     val scope = rememberCoroutineScope()
-    val context = LocalContext.current
 
     LaunchedEffect(Unit) { actions(HomeActions.CheckIfUserIsLogged) }
 
     Scaffold(
+        topBar = {
+            user?.let {
+                Header(it) { navigate(Screens.ProfileScreen) }
+            } ?: HomeTopBar()
+        },
         snackbarHost = { SnackbarHost(snackbarHostState) },
         floatingActionButton = {
             FloatingActionButton(
@@ -124,21 +126,6 @@ fun HomeScreen(
                             modifier = Modifier.padding(start = 10.dp),
                             contentPadding = PaddingValues(bottom = 100.dp)
                         ) {
-
-                            user?.let {
-                                item(
-                                    span = {
-                                        GridItemSpan(maxLineSpan)
-                                    }
-                                ) {
-                                    Header(
-                                        user = it,
-                                        context = context,
-                                        onClick = { navigate(Screens.ProfileScreen) }
-                                    )
-
-                                }
-                            }
                             items(localSketches.size) { index ->
                                 val sketch = localSketches[index]
                                 SketchPoster(
